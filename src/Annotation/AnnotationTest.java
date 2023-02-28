3f1a3b999e139eb3100bd53927cc4e2598762f36
Tian <t17861503086@163.com>
2023-02-28 19:11:02 +0800

    自定义注解及其实现

---
+++ src/Annotation/AnnotationTest.java
@@ -0,0 +1,18 @@
+package Annotation;
+
+public class AnnotationTest
+{
+    public static void main(String[] args) throws IllegalAccessException
+    {
+        Person person = new Person();
+        person.setName("nh");
+        person.setAge(10);
+        person.setId("001");
+
+        String validateField = LengthValidator.validateField(person);
+        if (validateField == null)
+            System.out.println(person);
+        else
+            System.out.println(validateField);
+    }
+}

163cd6dd37d50a5c33d0cafa951243175b4769c1
Tian <t17861503086@163.com>
2023-03-17 12:41:49 +0800

    抽象类继承

---
+++ src/Extends/demoExtends.java
@@ -0,0 +1,29 @@
+package Extends;
+
+abstract class demo
+{
+	public demo()
+	{
+		// TODO Auto-generated constructor stub
+		System.out.println("ceshi");
+	}
+}
+
+class demo1 extends demo
+{
+
+}
+
+// 抽象类可以有多个继承
+class demo2 extends demo
+{
+
+}
+
+public class demoExtends
+{
+	public static void main(String[] args)
+	{
+		demo1 d = new demo1();
+	}
+}
